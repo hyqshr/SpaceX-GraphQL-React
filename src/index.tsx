9cb2536b9f30e4af0f1b53dee204582e7c532bff
Yiqiu <hyiqiu23@gmail.com>
2023-10-12 18:40:42 -0400

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,24 @@ import Capsule from "pages/Capsule";
 import Report from "pages/Report";
 import { Provider } from "react-redux";
 import store from './store/store'; 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://f4c7f260c9fea0e71927a249d5966a2f@o4506039707172864.ingest.sentry.io/4506039709270016",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
